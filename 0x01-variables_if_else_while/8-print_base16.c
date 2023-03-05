8a63bdb03b6ccf53f03d81e1ae7a44699f757ba2
Magnettah <sm.mutindi@gmail.com>
2023-03-05 12:39:53 -0800

    Task 8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,7 +12,7 @@ int main(void)
 	char ch;
 
 	for (num = 0; num <= 9; num++)
-		putchar((num%10) + '0');
+		putchar((num % 10) + '0');
 	for (ch = 'a'; ch <= 'f'; ch++)
 		putchar(ch);
 	putchar('\n');
