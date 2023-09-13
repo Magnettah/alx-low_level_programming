128401c8b2e1e36d00894d310c592636cb08a2c1
Magnettah <sm.mutindi@gmail.com>
2023-09-12 20:27:36 -0700

    Task 3. Function-like macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file the program was computed from
+ * Return: always zero
+ *
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
