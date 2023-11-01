78e2249133f83339937d6bf9a33e763f6e408184
Magnettah <sm.mutindi@gmail.com>
2023-10-31 19:44:52 -0700

    Task 0: A function that creates an array of chars and initailizes it with a specific char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * creat_array - creates an array of size (size) and assigns character c
+ * @size: size of array
+ * @c: char to assign to the array
+ *
+ * Return: Pointer to an array, NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+
+	unsigned int a;
+
+	str = malloc(sizeof(char) * size);
+
+	if (size == 0 || str == NULL)
+
+		return (NULL);
+
+	for (a = 0; a < size; a++)
+
+		str[a] = c;
+
+	return (str);
+}
---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: Character to print
+ *
+ * Return: On success 1 and -1 on error
+ */
+
+int _putchar(cahr c)
+{
+	return (write(1, &c, 1));
+}
