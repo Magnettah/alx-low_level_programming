f9cda613628ceeb5dc1f03abc40018df2329020a
Magnettah <sm.mutindi@gmail.com>
2023-11-02 19:58:59 -0700

    Task 0: A function that allocates memory using malloc.

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - cause normal process termination with a status value of 98cause normal process termination with a status value of 98
+ * @b: allocated memory
+ *
+ * Return: A pointer to the allocated memory
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *m;
+
+	m = malloc(b);
+	if (m == NULL)
+		exit(98);
+	return (m);
+}
