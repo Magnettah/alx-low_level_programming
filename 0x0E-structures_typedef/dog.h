4907ec76b7673865f035094318e51dd1cb49c2be
Magnettah <sm.mutindi@gmail.com>
2023-11-06 18:16:54 -0800

    Task 0: Define a new type struct dog with the elements name, age and owner

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog structure
+ * @name: nameof a dog
+ * @age: the age of a dog
+ * @owner: name of the owner of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
