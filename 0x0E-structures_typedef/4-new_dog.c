6b7fb0836e69e941df2e7329a805c2da6a681f8e
Magnettah <sm.mutindi@gmail.com>
2023-11-08 03:11:24 -0800

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,10 +13,10 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = malloc(sizeof(dog_t));
-
 	if (name == NULL || owner == NULL)
 		return (NULL);
+
+	dog_t *new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 		return (NULL);
