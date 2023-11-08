eaa2df519d2f85c87465f6849920e67c4a74068f
Magnettah <sm.mutindi@gmail.com>
2023-11-08 02:08:21 -0800

    Update task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,18 +7,12 @@
  * @owner: The owner of a dog.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - Typedef for struct dog (used to define a new type for struct dog
- */
-
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
