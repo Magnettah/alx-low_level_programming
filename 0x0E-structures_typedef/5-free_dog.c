b610f27f8bb1d4dc7788979d12318e265f3464d8
Magnettah <sm.mutindi@gmail.com>
2023-11-08 04:10:20 -0800

    Task 5: A function that frees dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,17 +1,18 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
 
 /**
- * free_dog - freee the memory alloacted for struct dog
+ * free_dog - frees the memory alloacted for struct dog
  * @d: struct dog that is to be freed
  */
 
 void free_dog(dog_t *d)
 {
-	if (d)
+	if (d != NULL)
 	{
 		free(d->name);
 		free(d->owner);
 		free(d);
-			}
+	}
 }
