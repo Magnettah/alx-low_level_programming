c3413d4fd5c11914220e1aecefe412f88b676793
Magnettah <sm.mutindi@gmail.com>
2023-11-21 05:36:47 -0800

    Frres a list and sets the head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list and sets the head to NULL
+ * @head: pointer to the head of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *first = *head;
+	listint_t *next_node;
+
+	while (first != NULL)
+	{
+		next_node = first->next;
+		free(first);
+		first = next_node;
+	}
+
+	head = NULL;
+}
